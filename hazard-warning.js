a3dfca0ea1842db8a96a442ec80b05ad2f71d3da
Kevin Murphy <midimurphdesigns@gmail.com>
2018-02-20 14:38:36 -0700

    added bonus part about time or times

--- hazard-warning.js
+++ hazard-warning.js
@@ -1,9 +1,16 @@
 function hazardWarningCreator(typeOfWarning) {
     let warningCounter = 0;
+    let time = '';
+    
     return (location) => {
         warningCounter++;
+        if (warningCounter > 1) {
+            time = 'times';
+        } else {
+            time = 'time';
+        }
         console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}`);
-        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} ${time} today!`);
     }
 }
 
@@ -12,7 +19,7 @@ let snowWarning = hazardWarningCreator('It\'s snowing outside');
 let heatWarning = hazardWarningCreator('It\'s hot as hell');
 
 rocksWarning('Main st. pacific ave');
-snowWarning('Church st. and 7th');
+rocksWarning('Church st. and 7th');
 heatWarning('Cactus and 19th');
 
 // let test = ;
